54d825a5c8a76d0a04cc799524dc7ec7c5c574b2
Jörg Aderhold <joerg@jaderbass.de>
2026-07-14 12:06:07 +0200

    fix: reload.html to reload.php

---
+++ kap1/01-reload-vs-ajax/reload.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+  <meta charset="utf-8">
+  <title>Reload</title>
+</head>
+<link rel="stylesheet" href="../../css/style.css">
+
+<body>
+
+  <header class="container">
+    <h1>Seitenreload</h1>
+  </header>
+
+  <main class="container">
+    <p>Aktuelle Uhrzeit:</p>
+    <code><?php echo date('H:i:s'); ?></code>
+    <p><a href="">Seite neu laden</a></p>
+  </main>
+
+  <footer class="container footer">
+    <p>Kursmaterial – AJAX & jQuery</p>
+  </footer>
+
+</body>
+
+</html>
